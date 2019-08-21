de725e7cc513a76d29b9c3fa2837619637408129
zexceed12300 <36181923+zexceed12300@users.noreply.github.com>
2019-08-21 18:14:37 +0700

    fix nl80211 testmode command compile error

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -6547,7 +6547,7 @@ static int nl80211_testmode_do(struct sk_buff *skb, struct genl_info *info)
 
 	err = -EOPNOTSUPP;
 	if (rdev->ops->testmode_cmd) {
-		rdev->cur_cmd_info = info
+		rdev->cur_cmd_info = info;
 		err = rdev_testmode_cmd(rdev,
 				nla_data(info->attrs[NL80211_ATTR_TESTDATA]),
 				nla_len(info->attrs[NL80211_ATTR_TESTDATA]));
